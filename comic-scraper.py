ca96e0198de1fabb1b90e23521e30ec324f08f9b
Priya Shrinithi <47693475+PriyaShrinithi@users.noreply.github.com>
2020-11-20 16:03:57 +0530

    Add files via upload

--- comic-scraper.py
+++ comic-scraper.py
@@ -12,7 +12,7 @@ false = False
 pages = set()
 page = set()
 chapters = set()
-comic_path = 'https://www.mangareader.net/shingeki-no-kyojin'
+comic_path = 'https://www.mangareader.net/acmagame'
 
 comic = urllib.request.urlopen(comic_path)
 soup = BeautifulSoup(comic, 'html.parser')
@@ -24,10 +24,16 @@ comic_directory = 'Comics'
 manga_directory = soup.find(class_='d40').text.split(' Manga')[0]
 parent_directory = os.path.join(drive, comic_directory, manga_directory)
 print(manga_directory)
-if not os.path.exists(parent_directory):
-    os.mkdir(parent_directory)
 
-#path = 
+if not os.path.exists(parent_directory):
+    try:
+        os.mkdir(parent_directory)
+    except:
+        manga_directory = ' '.join(manga_directory.split(':'))    
+        parent_directory = os.path.join(drive, comic_directory, manga_directory)
+        if not os.path.exists(parent_directory):
+            os.mkdir(parent_directory)
+    
 chap = list()
 
 def get_chapters():
@@ -42,18 +48,21 @@ def get_chapters():
     toc_ = time.time()
     print('get chapters: ',(toc_ - tic_)*1000, ' ms')
 
-def get_page(ch_no):
+def get_chapter(ch_no):
     tic_ = time.time()
-    if str(ch_no) not in chap:
-        if chap[ch_no-1] in chap:
-            get_page(int(chap[ch_no-1]))
-        else:
-            print('Done!')
-            exit()
+    try:
+        ch = chap[ch_no - 1]
+    except:
+        print('Done!')
+        exit()
         
-    if ch_no not in chapters:
-        chapters.add(ch_no)
-        comic = urllib.request.urlopen(comic_path+'/'+str(ch_no))
+    if str(ch) in chap:
+        get_page(ch, ch_no)
+        
+def get_page(ch, chap_index):    
+    if ch not in chapters:
+        chapters.add(ch)
+        comic = urllib.request.urlopen(comic_path+'/'+str(ch))
         soup = BeautifulSoup(comic, 'html.parser')
         pg_links = soup.find(id='main').find('script')
         pg_links = str(pg_links)
@@ -62,11 +71,11 @@ def get_page(ch_no):
             pages = pages[2].split('=')
         except:
             print('Checking for continuity errors...')
-            get_page(ch_no+1)
+            get_chapter(chap_index+1)
         pages = pages[0].split(']')
         pages = pages[0]
         pages = pages.split('},')
-        ch_path = os.path.join(parent_directory, str(ch_no))
+        ch_path = os.path.join(parent_directory, str(ch))
         pg_no = 1
         
         for pg in pages:
@@ -80,7 +89,7 @@ def get_page(ch_no):
                     download_page(pg, pg_no, ch_path)
                     page.add(pg)
             pg_no+=1
-        get_page(ch_no+1)
+        get_chapter(chap_index+1)
     toc_ = time.time()
     print('get page: ', (toc_ - tic_)*1000, ' ms')
             
@@ -109,8 +118,7 @@ def download_page(page_path, pg_no, ch_path):
 #def get_page_url(pg_no):
     
 get_chapters()
-tqdm(get_page(1))
+tqdm(get_chapter(1))
 toc = time.time()
 print('comic-scraper: ',(toc-tic)*1000, ' ms')
 # DND, except chapter number and mangalink
-
