8257b3c0d90ed4c58fc848a6923fc02ed13a9af3
PriyaShrinithi <rps.24.2000@gmail.com>
2021-01-19 16:39:37 +0530

    new file:   __pycache__/shared.cpython-37.pyc 	modified:   comic-scraper.py

--- comic-scraper.py
+++ comic-scraper.py
@@ -1,18 +1,16 @@
-import time
 import os
-from bs4 import BeautifulSoup
-import urllib.request
-import requests
 import shutil
-import json
+import time
+
+import numpy as np
+import requests
 import shared as sh
 
 true = True
 false = False
 
 global soup
-headers = {"User-Agent": "Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 ("
-                         "KHTML, like Gecko) Version/4.0 Safari/534.30"}
+
 
 def get_manga_name(manga, chapter):
     comic_path = 'https://img.mghubcdn.com/file/imghub'
@@ -28,7 +26,7 @@ def get_chapter(comic_path, chapter):
     comic_path += '/' + str(chapter)
     get_page(comic_path, 1, chapter)
     print(comic_path, chapter)
-    print("Chapter: "+chapter)
+    print("Chapter: " + chapter)
 
 
 def get_page(comic_path, page, chapter):
@@ -38,6 +36,8 @@ def get_page(comic_path, page, chapter):
 
 
 def download_page(chapter, page, comic_path):
+    global d
+    d = 0
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 ("
                       "KHTML, like Gecko) Version/4.0 Safari/534.30"}
@@ -45,18 +45,18 @@ def download_page(chapter, page, comic_path):
     print(response.status_code)
     if response.status_code == 404:
         if page != 1:
-            comic_path = comic_path.split('/')[:-2]
+            comic_path = comic_path.split('/')[:-3]
             comic_path = '/'.join(comic_path)
             get_chapter(comic_path, chapter + 1)
         else:
-        # delete last made directory and exit
+            # delete last made directory and exit
             exit()
     else:
         chap = sh.chapter_directory(chapter)
-        filename = os.path.join(chap, str(page)+'.jpg')
+        filename = os.path.join(chap, str(page) + '.jpg')
         print(filename)
         if os.path.exists(filename):
-            get_page(comic_path, page+1, chapter)
+            get_page(comic_path, page + 1, chapter)
         else:
             response.raw.decode_content = true
             with open(filename, 'wb') as f:
@@ -65,30 +65,10 @@ def download_page(chapter, page, comic_path):
         comic_path = comic_path.split('/')[:-1]
         comic_path = '/'.join(comic_path)
         get_page(comic_path, page + 1, chapter)
-    '''
+        d += 1
+        if d % 50 == 0:
+            delay = np.random.random(1000000)
+            time.sleep(delay)
 
 
-def download_page(page_path, pg_no, ch_path):
-    print(ch_path)
-    tic_ = time.time()
-    if not os.path.exists(ch_path):
-        os.mkdir(ch_path)
-    filename = os.path.join(ch_path, str(pg_no) + '.jpg')
-    if not os.path.exists(filename):
-        req = requests.get(page_path, stream=true)
-        req.raw.decode_content = true
-        if req.status_code == 200:
-            with open(filename, 'wb') as f:
-                shutil.copyfileobj(req.raw, f)
-            print('Download Successful!')
-        else:
-            print(req.status_code)
-            print('ch: ' + ch_path + ' pg: ' + str(pg_no) + ' Downloading Again...')
-            download_page(page_path, pg_no, ch_path)
-    else:
-        print('File Exists')
-    toc_ = time.time()
-    print('download page: ', (toc_ - tic_) * 1000)'''
-
-
-get_manga_name("one-piece", chapter=1001)
+get_manga_name("hajime-no-ippo", chapter=1)
